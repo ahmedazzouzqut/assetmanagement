7a6d75c59e03c42101271505e5519c21c75bbb9b
Ahmed Azzouz <n11450771@qut.edu.au>
2025-08-15 17:11:25 +1000

    ASSET-45 Sub Task 7.1: Create a form component for asset assignment form

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+// create an axios instance to url
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
   baseURL: 'http://3.27.181.204:5001', // live
