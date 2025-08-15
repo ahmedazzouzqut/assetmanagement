d4ca6d2adbc2e6dde79e002567944570fc314681
Ahmed Azzouz <n11450771@qut.edu.au>
2025-08-15 20:15:54 +1000

    ASSET-84 Sub Task 16.2: Implement functionality to update assets through the backend API.

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,8 @@ import axios from 'axios';
 // create an axios instance to Ip address 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://13.239.234.98:5001', // live
+  //baseURL: 'http://13.239.234.98:5001', // live
+  baseURL: 'http://3.106.127.210:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
