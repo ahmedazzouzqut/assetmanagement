ae2052ff632da2f6a7d0aee477bba769ada396ad
Ahmed Azzouz <n11450771@qut.edu.au>
2025-08-15 18:40:55 +1000

    ASSET-47 Sub Task 7.3: Connect the asset assignment form to the backend API.

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 // create an axios instance to Ip address
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://3.27.181.204:5001', // live
+  baseURL: 'http://13.239.234.98:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
