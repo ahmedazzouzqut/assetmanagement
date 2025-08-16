19e7dcfe24cd65d8487c24c89337514c4c20a5c7
Ahmed Azzouz <n11450771@qut.edu.au>
2025-08-16 10:52:31 +1000

    ASSET-48 Sub Task 8.1: Create a form component for asset transfer/deallocation form

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 // create an axios instance to Ip address 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://13.239.234.98:5001', // QUT Instance
+  baseURL: 'http://3.25.67.165:5001', // QUT Instance
   //baseURL: 'http://3.106.127.210:5001', // My Instance
   headers: { 'Content-Type': 'application/json' },
 });
