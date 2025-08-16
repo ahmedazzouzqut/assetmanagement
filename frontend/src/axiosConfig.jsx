9130ea51b5b02877ab3d45afa4e5c744f89b0539
Ahmed Azzouz <n11450771@qut.edu.au>
2025-08-16 20:22:02 +1000

    TAS-30 ST 4.2: Connect the viewing component to the backend API to fetch tasks.

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 // create an axios instance to Ip address 
 const axiosInstance = axios.create({
-  //baseURL: 'http://localhost:5001', // local
+  //baseURL: 'http://localhost:5001', // local version
   baseURL: 'http://3.27.184.113:5001', // QUT Instance
   //baseURL: 'http://3.106.127.210:5001', // My ec2 Instance
   headers: { 'Content-Type': 'application/json' },
