10b19d06fe78f27faf70139a3bf0d1ada7ec7324
Ahmed Azzouz <n11450771@qut.edu.au>
2025-08-27 17:24:02 +1000

    TAS-38 ST 6.4: Implement success and error handling for profile updates.

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 // create an axios instance to Ip address 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local version
-  baseURL: 'http://3.27.184.113:5001', // QUT Instance
+  baseURL: 'http://52.65.230.196:5001', // QUT Instance
   //baseURL: 'http://3.106.127.210:5001', // My ec2 Instance
   headers: { 'Content-Type': 'application/json' },
 });
